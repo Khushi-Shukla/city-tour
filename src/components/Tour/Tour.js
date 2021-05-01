74e46012f652f1b8817614db0a26e1084b98241b
unknown <tusharika.shukla05@gmail.com>
2021-05-02 02:04:23 +0530

    Event Handling

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -1,24 +1,32 @@
 import React, { Component } from 'react'
 import './Tour.scss'
 export default class Tour extends Component {
+    state={
+        showInfo: false
+    };
+    handleInfo=()=>{
+        this.setState({showInfo:!this.state.showInfo})
+    }
+    
     render() {
+         const{id, city, img, name, info}= this.props.tour;
+         const{removeTour} = this.props;
         return (
             <article className="tour">
                  <div className="img-container">
-                     <img src="https://images.pexels.com/photos/4210037/pexels-photo-4210037.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="img" />
-                 <span className="close-btn">
+                     <img src={img} alt="img" />
+                 <span className="close-btn" onClick={()=>removeTour(id)}>
                      {/* <i className="fas fa-window-close"></i> */}
                      <i className="fas fa-window-close"></i>
                  </span>
                  </div>
                  <div className="tour-info">
-                     <h3>city</h3>
-                     <h4>name</h4>
-                     <h5>info<span><i className="fas fa-caret-square-down"/></span></h5>   
-                     <p>
-                         lorem lispjdsfhnkdjn dfkjsdj  sdfuhsdk
-                          dfhsd dfhk akfdkhj sdfh k fsidfuh sd hk ds
-                     </p>
+                     <h3>{city}</h3>
+                     <h4>{name}</h4>
+                     <h5>info{" "}
+                     <span onClick={()=>this.handleInfo()}>
+                         <i className="fas fa-caret-square-down"/></span></h5>   
+                     {this.state.showInfo && <p>{info}</p>}
                  </div>
                 single tour
             </article>
